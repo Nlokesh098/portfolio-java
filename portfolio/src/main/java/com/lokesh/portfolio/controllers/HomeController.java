56a8e3a1b2630eee174a6362efe38c677a3f3c72
Nlokesh Nelluri <lokeshnelluri9@gmail.com>
2026-01-25 14:08:47 -0600

    resolved comments

--- portfolio/src/main/java/com/lokesh/portfolio/controllers/HomeController.java
+++ portfolio/src/main/java/com/lokesh/portfolio/controllers/HomeController.java
@@ -10,8 +10,6 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.SQLOutput;
 import java.util.List;
 // import ;
 
