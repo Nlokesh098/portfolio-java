eb5111a721112983f9afd839d7e5403ae42eccc6
Nlokesh Nelluri <lokeshnelluri9@gmail.com>
2026-01-25 14:03:23 -0600

    print

--- portfolio/src/main/java/com/lokesh/portfolio/controllers/HomeController.java
+++ portfolio/src/main/java/com/lokesh/portfolio/controllers/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLOutput;
 import java.util.List;
 // import ;
 
@@ -33,6 +34,7 @@ public class HomeController {
         this.mRepo = mRepo;
     }
 
+
     //setter injection is performed here.
 //    private MovieServImpl msi;
 //    @Autowired
@@ -49,6 +51,7 @@ public class HomeController {
     @GetMapping ("/movie")
     public ResponseEntity<List<Movie>> fetchById(){
         mRepo.findAll();
+        System.out.println("sample");
         return new ResponseEntity<>(mRepo.findAll(), HttpStatus.OK);
         //return msi.fetchMovie("lokk");
     }
